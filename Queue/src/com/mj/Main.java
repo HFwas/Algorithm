59995ce5f7c4140c58394567ad648b49f1f31d4e
HFwas <1558934404@qq.com>
2020-06-07 15:33:36 +0800

    "2020.6.7:循环队列"

--- Queue/src/com/mj/Main.java
+++ Queue/src/com/mj/Main.java
@@ -1,8 +1,32 @@
 package com.mj;
 
+import com.mj.circle.CircleQueue;
+
 public class Main {
 
 	public static void main(String[] args) {
+		test02();
+		
+	}
+	
+	static void test02(){
+		CircleQueue<Integer> queue = new CircleQueue<>();
+		for (int i = 0; i < 10; i++) {
+			queue.enQueue(i);
+		}
+		for (int i = 0; i < 5; i++) {
+			queue.deQueue();
+		}
+		for (int i = 15; i < 23; i++) {
+			queue.enQueue(i);
+		}
+		System.out.println(queue);
+		while (!queue.isEmpty()) {
+			System.out.println(queue.deQueue());
+		}
+	}
+	
+	static void test01(){
 		Deque<Integer>queue = new Deque();
 		queue.enQueueFront(11);
 		queue.enQueueFront(22);
@@ -12,16 +36,17 @@ public class Main {
 		while (!queue.isEmpty()) {
 			System.out.println(queue.deQueueFront());
 		}
-		
-		
-		/*Queue<Object> queue = new Queue<>();
+	}
+	
+	static void test03(){
+		Queue<Object> queue = new Queue<>();
 		queue.enQueue(11);
 		queue.enQueue(22);
 		queue.enQueue(33);
 		queue.enQueue(44);
 		while (!queue.isEmpty()) {
 			System.out.println(queue.deQueue());
-		}*/
+		}
 	}
 
 }
---
+++ Queue/src/com/mj/circle/CircleQueue.java
@@ -0,0 +1,81 @@
+package com.mj.circle;
+
+//环形队列
+public class CircleQueue<E> {
+	private int front;
+	private int size;
+	private E[] elements;
+	
+	private static final int DEFAULT_CAPACITY = 10;
+
+	public CircleQueue() {
+		elements = (E[]) new Object[DEFAULT_CAPACITY];
+	}
+	
+	// 入队
+	public void enQueue(E element) {
+		ensureCapacity(size+1);
+		elements[index(size)] = element;
+		size++;
+	}
+
+	// 出队
+	public E deQueue() {
+		E frontElement = elements[front];
+		elements[front] = null ;
+		front = index(1);
+		size--;
+		return frontElement;
+	}
+
+	// 元素数量
+	public int size() {
+		return size;
+	}
+
+	public E front() {
+		return elements[front];
+	}
+
+	// 是否为空
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public int index(int index) {
+		return (index +front) % elements.length;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder string = new StringBuilder();
+		string.append(" capacity=").append(elements.length)
+		.append(" size=").append(size)
+		.append(" front=").append(front)
+		.append(", [");
+		for (int i = 0; i < elements.length; i++) {
+			if (i != 0){
+				string.append(", ");
+			}
+			string.append(elements[i]);
+		}
+		string.append("]");
+		return string.toString();
+	}
+	
+	//扩容操作
+	private void ensureCapacity(int capacity){
+		int oldCapacity = elements.length;
+		if(oldCapacity >= capacity) return;
+		// 新容量为旧容量的1.5倍
+		int newCapacity = oldCapacity + (oldCapacity >> 1);
+		E[] newElements = (E[])new Object[newCapacity];
+		for (int i = 0; i < size; i++) {
+			newElements[i] = elements[index(i)]; // 拷贝原数组元素到新数组
+		}
+		elements = newElements;
+
+		//重置front
+		front = 0;
+	}
+}

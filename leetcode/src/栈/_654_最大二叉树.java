7cd6af0eebe37988a666c652f9ef2abe5756ed21
HFwas <1558934404@qq.com>
2020-08-07 21:31:09 +0800

    "2020-8-7:leetcode栈队列刷题"

---
+++ leetcode/src/栈/_654_最大二叉树.java
@@ -0,0 +1,19 @@
+package 栈;
+/**
+ * https://leetcode-cn.com/problems/maximum-binary-tree/
+ * @author Administrator
+ *输入：[3,2,1,6,0,5]
+输出：返回下面这棵树的根节点：
+      6
+    /   \
+   3     5
+    \    / 
+     2  0   
+       \
+        1
+ */
+public class _654_最大二叉树 {
+//	public TreeNode constructMaximumBinaryTree(int[] nums) {
+//		
+//    }
+}
---
+++ leetcode/src/栈/_739_每日温度.java
@@ -0,0 +1,17 @@
+package 栈;
+/**
+ * https://leetcode-cn.com/problems/daily-temperatures/
+ * @author Administrator
+ *给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，
+ *你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+ */
+public class _739_每日温度 {
+	public int[] dailyTemperatures(int[] T) {
+		
+		
+		
+		
+		
+		return null;
+    }
+}
